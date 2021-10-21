9ef8948b951a166fc8a25121913487594a7dc8b0
Christian Helle <christian.helle@outlook.com>
2021-10-22 00:34:32 +0200

    Initial commit

---
+++ etl/test_basic_ingestion.py
@@ -0,0 +1,53 @@
+import unittest
+from dataclasses import dataclass
+from typing import Dict
+from unittest.mock import MagicMock
+
+from atc.etl import Extractor, Loader, Orchestration, MultiInputTransformer
+from autofaker import Autodata
+from pyspark.sql import DataFrame
+
+
+class TimestampTransformer(MultiInputTransformer):
+    def process_many(self, dataset: Dict[str, DataFrame]) -> DataFrame:
+        df: DataFrame = None
+        for k in dataset:
+            df = dataset[k].union(df)
+        return df
+
+
+class PassLoader(Loader):
+    def save(self, df: DataFrame) -> DataFrame:
+        return df
+
+
+class BasicIngestionTestCase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        @dataclass
+        class Person:
+            id: int
+            name: str
+            address: str
+
+        extractor: Extractor = MagicMock()
+        extractor.read.return_value = Autodata.create_spark_dataframe(Person, use_fake_data=True)
+
+        df = (Orchestration
+              .extract_from(extractor)
+              .extract_from(extractor)
+              .transform_with(TimestampTransformer())
+              .load_into(PassLoader())
+              .build()
+              .execute())
+
+        df.show()
+
+        cls.result = df
+
+    def test_returns_dataframe(self):
+        self.assertIsNotNone(self.result)
+
+    def test_returned_dataframe_contains_timestamp_column(self):
+        self.assertTrue(self.result.columns.__contains__('timestamp'))
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='autofaker-examples',
+    version='0.1.0',
+    packages=[''],
+    url='https://github.com/christianhelle/autofaker-examples',
+    license='',
+    author='Christian Helle',
+    author_email='christian.helle@outlook.com',
+    description='Code examples for different usages of AutoFaker'
+)
